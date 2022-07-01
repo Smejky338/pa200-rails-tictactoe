b57f8ba82804c384bc258f6cfca91a3c54bf6852
Smejky338 <jsmejkal@redhat.com>
2022-07-01 10:17:39 +0200

    fix: missing route for turn
    (which is not in the basic CRUD)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :games
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post '/games/:id/turn', to: 'games#turn'
 end
